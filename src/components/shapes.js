e6ea22910cb41b8d055ce5491115b4a9348cb24e
LuismGil <luism6977@gmail.com>
2021-11-12 11:59:05 -0300

    Fixing functions

--- src/components/shapes.js
+++ src/components/shapes.js
@@ -1,20 +1,23 @@
 // Square code
+const inputSquare = document.getElementById('squareInput');
+const resultSquare = document.getElementById('squareResult');
+const btnSquarePerimeter = document.getElementById('squareBtnPerimeter');
+const btnSquareArea = document.getElementById('squareBtnArea');
 
-const btn = document.getElementById('squareBtn');
+btnSquarePerimeter.addEventListener('click',function() {
+  const valueSquarePerimeter = inputSquare.value;
+  const perimeterSquare = squarePerimeter(valueSquarePerimeter);
 
-btn.addEventListener('click',function() {
-  const input = document.getElementById('squareInput')
-  const value = input.value;
-  const perimeter = squarePerimeter(value);
-  console.log('perimetro = ', perimeter );
-  const area = squareArea(value);
-  const resultSquare = document.getElementById('squareResult')
-  resultSquare.innerHTML = `The perimeter is: ${perimeter}\ and the area is: ${area}`
-  console.log('Area = ', area);
-  console.log('Perimetro => ' + perimeter + ' Area => ' + area);
-  
+  resultSquare.innerHTML = `The Perimeter is: ${perimeterSquare}`;
 })
 
+
+btnSquareArea.addEventListener('click',function() {
+  const valueSquareArea = inputSquare.value;
+  const areaSquare = squareArea(valueSquareArea);
+
+  resultSquare.innerHTML = `The Area is: ${areaSquare}`;
+})
 
 function squarePerimeter(side) {
   return side * 4;
@@ -25,6 +28,32 @@ function squareArea(side) {
 }
 
 // Triangle code
+const inputA = document.getElementById('triangleA');
+const inputB = document.getElementById('triangleB');
+const inputBase = document.getElementById('triangleBase');
+const inputHeight = document.getElementById('triangleHeight');
+const resultTriangle = document.getElementById('triangleResult');
+const btnTrianglePerimeter = document.getElementById('triangleBtnPerimeter');
+const btnTriangleArea = document.getElementById('triangleBtnArea');
+
+
+btnTrianglePerimeter.addEventListener('click', function() {
+  const valueAPerimeter = parseInt(inputA.value);
+  const valueBPerimeter = parseInt(inputB.value);
+  const valueBasePerimeter = parseInt(inputBase.value);
+  const perimeterTriangle = trianglePerimeter(valueAPerimeter, valueBPerimeter, valueBasePerimeter);
+
+  resultTriangle.innerHTML = `The Perimeter is: ${perimeterTriangle}`;
+})
+
+
+btnTriangleArea.addEventListener('click', function() {
+  const valueBaseArea = parseInt(inputBase.value);
+  const valueHeightArea = parseInt(inputHeight.value);
+  const areaTriangle = triangleArea(valueBaseArea, valueHeightArea);
+
+  resultTriangle.innerHTML = `The Area is: ${areaTriangle}`;
+})
 
 function trianglePerimeter(side1, side2, base) {
   return side1 + side2 + base;
@@ -36,6 +65,24 @@ function triangleArea(base, height) {
 
 // Circle code
 const PI = Math.PI;
+const inputCircle = document.getElementById('inputCircle');
+const resultCircle = document.getElementById('circleResult');
+const btnCirclePerimeter = document.getElementById('circleBtnPerimeter');
+const btnCircleArea = document.getElementById('circleBtnArea');
+
+btnCirclePerimeter.addEventListener('click', function() {
+  const valueCirclePerimeter = parseInt(inputCircle.value);
+  const perimeterCircle = circlePerimeter(valueCirclePerimeter).toFixed(2);
+
+  resultCircle.innerHTML = `The Perimeter is: ${perimeterCircle}`
+})
+
+btnCircleArea.addEventListener('click', function() {
+  const valueCircleArea = parseInt(inputCircle.value);
+  const areaCircle = circleArea(valueCircleArea).toFixed(2);
+
+  resultCircle.innerHTML = `The Area is: ${areaCircle}`
+})
 
 function circleDiameter(radius) {
   return radius * 2;
